74a57fcf32401fd811b201e141b39f0d94c7b974
Kadotchi <kadotchi724@gmail.com>
2022-08-11 23:21:37 +0900

    ✨ 挙動を確かめる用のPageを追加

---
+++ src/pages/sample.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import React from 'react'
+
+export default function sample() {
+  return (
+    <div>
+      <header>
+        <h1>Shimabu IT University</h1>
+      </header>
+    </div>
+  )
+}
