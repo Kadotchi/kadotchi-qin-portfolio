0cd493388187fb0dbdf3e025307616f44588b861
Kadotchi <kadotchi724@gmail.com>
2022-08-13 23:37:50 +0900

    ✨ 各ページのLInkを作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,18 @@ export default function Header(props: string) {
   return (
     <div>
       <h1>{props.header}</h1>
-      {/* <Link href={/about}><a>About</a></Link> */}
+      <Link href='/about'>
+        <a>About</a>
+      </Link>
+      <Link href='/blog'>
+        <a>Blog</a>
+      </Link>
+      <Link href='/portfolio'>
+        <a>Portfolio</a>
+      </Link>
+      <Link href='/contact'>
+        <a>Contact</a>
+      </Link>
     </div>
   )
 }
