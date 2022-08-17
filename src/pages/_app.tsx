bd532ffc58a691e7cf0736c5e09d2f54fa72d610
Kadotchi <kadotchi724@gmail.com>
2022-08-17 23:20:01 +0900

    ✨ ヘッダーをグローバルで追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '@/components/Header'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header title='Shimabu IT University' />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
